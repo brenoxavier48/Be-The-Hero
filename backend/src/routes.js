e723695c08676a64d91b375b2bc0d1b590722e61
BrenoXavier <brenoxavier8.bx@gmail.com>
2020-04-04 17:34:03 -0300

    Alteração nas rotas

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -6,12 +6,31 @@ const incidentsController = require('./controllers/incidentsController.js');
 const profileController = require('./controllers/profileController.js');
 const sessionController = require('./controllers/sessionController.js');
 
+const { celebrate, Segments, Joi } = require('celebrate');
+
+const validationOng = {
+    [Segments.BODY]: Joi.object().keys({
+        name: Joi.string().required(),
+        email: Joi.string().required().email(),
+        whatsapp: Joi.number().required().min(10).max(11),
+        city: Joi.string().required(),
+        uf: Joi.string().required().length(2)
+    })
+}
+
+const validationProfile = {
+    [Segments.HEADERS]: Joi.object({
+        authorization: Joi.string().required(),
+    }).unknown(),
+}
+
+
 routes.get('/ongs', ongController.index);
-routes.post('/ongs', ongController.insert);
+routes.post('/ongs', celebrate(validationOng), ongController.insert);
 
 routes.post('/session', sessionController.validateOngSession);
 
-routes.get('/profile', profileController.indexIncidentsByOng);
+routes.get('/profile', celebrate(validationProfile), profileController.indexIncidentsByOng);
 
 routes.get('/incidents', incidentsController.index);
 routes.post('/incidents', incidentsController.insert);
